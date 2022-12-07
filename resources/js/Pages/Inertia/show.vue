cb8144f2ebbaf60e2d6f8ec8bc1fd709a32f267c
DESKTOP-ONT25U1\skrgb <skrgbiz168@gmail.com>
2022-12-07 22:03:31 +0900

    inertia-sec02_inertia_vuejs

---
+++ resources/js/Pages/Inertia/show.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia';
+
+defineProps({
+    id:String,
+    blog: Object
+})
+
+const deleteComform = (id) =>{
+    console.log(id)
+    Inertia.delete(`/inertia/${id}`, {
+    onBefore: () => confirm('本当に削除しますか？')
+    })
+}
+</script>
+
+<template>
+    {{id}}
+    {{blog.id}}
+    <button @click="deleteComform(blog.id)">削除</button>
+</template>
